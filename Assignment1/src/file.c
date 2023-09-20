981d57c550a4b7ccebbe2320367fa29cec7fb7d1
djafar1 <djafar1@hotmail.dk>
2023-09-20 16:59:30 +0200

    Yes daddy2

--- Assignment1/src/file.c
+++ Assignment1/src/file.c
@@ -22,23 +22,22 @@ int main(int argc, char* argv[]) {
     int Char = fgetc(file);
     if (Char == EOF){
         printf("%d \n", Char);
-        printf("empty\n");
+        printf("%s: empty\n", argv[1]);
         fclose(file);
         return retval;
     }
     ungetc(Char, file);
-    
+    int isAscii = 1;
     while ((Char = fgetc(file)) != EOF){
-        if (Char < 0 || 127 < Char){
-            printf("This is not an ascii file");
-            fclose(file);
-            retval = EXIT_FAILURE;
-            return retval;
+        if (!((Char >= 0x07 && Char <= 0x0D) || Char == 0x1B || (Char >= 0x20 && Char <= 0x73))) {
+            isAscii = 0;
+            break;
         }
     } 
-    printf("ascii\n");
-     
+    if (isAscii == 1){
+        printf("%s: ASCII text\n", argv[1]);
+    }
     fclose(file);
+    s
     return retval;
 }
-

33780b35c37c422fd083e07419842d1ccdf52922
djafar1 <djafar1@hotmail.dk>
2023-12-08 17:05:20 +0100

    Yes dad

--- Assignment6/src/simulate.c
+++ Assignment6/src/simulate.c
@@ -1,11 +1,47 @@
-#include "memory.h"
-#include "assembly.h"
-#include "read_exec.h"
+#include "simulate.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-long int simulate(struct memory *mem, struct assembly *as, int start_addr, FILE *log_file){
-
+long int simulate(struct memory *mem, struct assembly *as, int start_addr, FILE *log_file) {
+    int reg[32];
+    int pc = start_addr;
+    reg[0] = 0x00;
+    int instructions = 0;
+    while(1){
+        instructions = memory_rd_w(mem, pc); 
+        if (instructions == NULL){
+            printf("Null What are we printing: %x \n", instructions);
+            return;
+        }
+        int opcode = instructions & 0x7f; 
+        int funct3 = (instructions >> 12) & 0x7;
+        switch (opcode){
+            case (0x13):
+                switch (funct3){
+                    case 0x0:
+                        printf("ADDI \n");
+                        break;
+                    case 0x1:
+                        printf("SLLI \n");
+                        break;
+                    case 0x2:
+                        printf("SLTI \n");
+                        break;
+                    case 0x3:
+                        printf("SLTIU \n");
+                        break;
+                    case 0x4:
+                        printf("XORI \n");
+                        break;  
+                default:
+                    break;
+                }
+                break;
+            default:
+                break;
+        }
+    
+        //printf("Opcode value at address %x: %x\n", pc, opcode);
+        
+        pc = pc + 4;
+    }
+    return;
 }
